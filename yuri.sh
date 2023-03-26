04d01efd8b52dfd54dd27d50d5a699ed2ff563d4
Zatiel <44403468+callmezatiel@users.noreply.github.com>
2023-03-26 08:16:05 -0600

    Add files via upload

---
+++ yuri.sh
@@ -0,0 +1,15 @@
+# Enter your password user when any command confirm request
+set -e
+# Upgrade
+sudo dnf upgrade -y 
+# RPM Fusion / NonFree
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+# Installing plugins for playing movies and music
+sudo dnf groupupdate core -y 
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+sudo dnf groupupdate sound-and-video  -y
+# Add flathub repo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
+# Increase DNF speed
+echo -e "# Added for speed: \nfastestmirror=True\nmax_parallel_downloads=5\nkeepcache=True" | sudo tee -a /etc/dnf/dnf.conf
