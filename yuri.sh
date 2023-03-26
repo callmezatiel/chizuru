18ba46b1b893d0424a89ab9d95ade42efb0b4887
Zatiel <44403468+callmezatiel@users.noreply.github.com>
2023-03-26 10:15:17 -0600

    Update yuri.sh

--- yuri.sh
+++ yuri.sh
@@ -7,8 +7,8 @@ sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-rele
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 # Installing plugins for playing movies and music
 sudo dnf groupupdate core -y 
-sudo dnf groupupdate multimedia -y
-sudo dnf groupupdate sound-and-video -y
+#sudo dnf groupupdate multimedia -y
+#sudo dnf groupupdate sound-and-video -y
 sudo dnf install dnf-plugins-core -y
 # Installing Nvidia Driver Akmod-Nvidia
 sudo dnf install akmod-nvidia -y
