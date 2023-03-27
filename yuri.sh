41107c235713fa925c3141a1e4606dda33150195
Zatiel <44403468+callmezatiel@users.noreply.github.com>
2023-03-26 21:34:12 -0600

    Update yuri.sh

--- yuri.sh
+++ yuri.sh
@@ -5,16 +5,17 @@ sudo dnf upgrade -y
 # RPM Fusion / NonFree
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-# Installing plugins for playing movies and music
 sudo dnf groupupdate core -y 
-#sudo dnf groupupdate multimedia -y
-#sudo dnf groupupdate sound-and-video -y
 sudo dnf install dnf-plugins-core -y
 # Installing Nvidia Driver Akmod-Nvidia
 sudo dnf install akmod-nvidia -y
 sudo dnf install xorg-x11-drv-nvidia-cuda -y
-# Add flathub repo
+# Add Flathub Repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
+# Add EPEL Repo
+sudo yum install -y epel-release yum-utils
+sudo yum-config-manager --enable epel
+sudo yum clean all && sudo yum update -y
 # Updating Firmware
 sudo fwupdmgr get-devices -y 
 sudo fwupdmgr refresh --force -y 
