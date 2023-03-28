be3412706dbe27154bc2652bfa7f040feff489a1
Zatiel <44403468+callmezatiel@users.noreply.github.com>
2023-03-28 03:38:52 -0600

    Update yuri.sh

--- yuri.sh
+++ yuri.sh
@@ -2,21 +2,32 @@
 set -e
 # Upgrade
 sudo dnf upgrade -y 
+
 # RPM Fusion / NonFree
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf groupupdate core -y 
 sudo dnf install dnf-plugins-core -y
+
 # Installing Nvidia Driver Akmod-Nvidia
 sudo dnf install akmod-nvidia -y
 sudo dnf install xorg-x11-drv-nvidia-cuda -y
+
 # Add Flathub Repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
+
+# Add EPEL Experimental
+cd /tmp/
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+sudo dnf install ./epel-release-latest-9.noarch.rpm --skip-broken
+
 # Updating Firmware
-sudo fwupdmgr get-devices -y 
-sudo fwupdmgr refresh --force -y 
-sudo fwupdmgr get-updates -y
-sudo fwupdmgr update -y
+#sudo fwupdmgr get-devices -y 
+#sudo fwupdmgr refresh --force -y 
+#sudo fwupdmgr get-updates -y
+#sudo fwupdmgr update -y
+# Increase DNF speed
+
 # Increase DNF speed
 echo -e "# Added for speed: \nfastestmirror=True\nmax_parallel_downloads=5\nkeepcache=True" | sudo tee -a /etc/dnf/dnf.conf
 # Clean Your System
